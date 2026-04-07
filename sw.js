e6fb3c652c517709251c7080ab659a877ebf8f23
test99od-eng <test99od@gmail.com>
2026-04-07 10:16:57 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,43 @@
-const CACHE_NAME = 'kgh-permanent-cache-v2';
+const CACHE_NAME = 'kgh-app-v5';
+const OFFLINE_PAGE = '/?m=1';
 
-self.addEventListener('install', e => {
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([OFFLINE_PAGE]);
+    })
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-
-  event.respondWith(
-    fetch(event.request).then(response => {
-      // បើមាន Net: បង្ហាញទិន្នន័យថ្មី និងលួច Save ទុកក្នុង Cache
-      const resClone = response.clone();
-      caches.open(CACHE_NAME).then(cache => {
-        cache.put(event.request, resClone);
-      });
-      return response;
-    }).catch(() => {
-      // បើអត់ Net: ទៅរកមើលក្នុង Cache
-      return caches.match(event.request).then(res => res || caches.match('/?m=1'));
-    })
-  );
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          // បើមាន Net: បង្ហាញទំព័រថ្មី និង Save ទុកក្នុង Cache
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        })
+        .catch(() => {
+          // បើអត់ Net: ទៅរកមើលទំព័រដែលធ្លាប់ Save ក្នុង Cache
+          return caches.match(event.request).then((res) => {
+            return res || caches.match(OFFLINE_PAGE);
+          });
+        })
+    );
+  } else {
+    // សម្រាប់រូបភាព ឬ CSS
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  }
 });
